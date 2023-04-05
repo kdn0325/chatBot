79486e4cb5c862811babda783afe25201841e5da
abel <dn10003@gmail.com>
2023-04-05 15:17:52 +0900

    #1_ChatGPT를 이용한 챗봇

--- src/screens/ChatBotStack/ChatBotHome/index.tsx
+++ src/screens/ChatBotStack/ChatBotHome/index.tsx
@@ -2,18 +2,14 @@ import { View, Text, SafeAreaView, FlatList, ScrollView ,ActivityIndicator } fro
 import React, { useEffect, useRef, useState } from 'react'
 import { NavigationProp } from '@/types'
 import Container from '@/layout'
-import ButtonBox from '@/components/ButtonBox'
-import { ContentArea, FooterArea, HeadArea, InputContainer, Submit, SubmitContainer, TouchButton } from './styles'
+import { ContentArea, FooterArea, HeadArea, InputContainer, SubmitContainer, TouchButton } from './styles'
 import { GlobalImage, GlobalText } from '@/styles/global'
 import Input from '@/components/Input'
 import SubmitIco from '@/assets/img/SubmitIco.png';
-import Modal from 'react-native-modal'
-import { getFormSubmit } from '@/utils/apiClient'
 import axios from 'axios'
-import Loading from '@/components/Loading'
 import theme from '@/styles/theme'
+import { getFormSubmit } from '@/utils/apiClient'
 
-// 
 
 interface Chat {
   id: number;
@@ -64,32 +60,27 @@ const ChatBotHome = ({navigation}:NavigationProp) => {
         }
       });
       const text = res.data.choices[0].text;
-      console.log('res.headers.date?' ,res.headers.date)
+      const time = res.data.created;
+      console.log('res.headers.date?' ,res.data.created)
       console.log('res.data?' ,res.data)
-      setSubmitDate(Timeend(res.headers.date).toString)
-      setCreateDate(Unix_timestamp(res.data.created))
       
-      const curr = new Date();
-      setData([...data,{type:'user','text':textInput},{type:'bot','text':text}])
+      setData([...data,{type:'user','text':textInput,'time':Unix_timestamp(time)},{type:'bot','text':text,'time':Unix_timestamp(time)}])
       setLoading(false);
       setTextInput('');
     }
 
     const Unix_timestamp = (t)=> {
-      var date = new Date(t*1000);
-      var year = date.getFullYear();
-      var month = "0" + (date.getMonth()+1);
-      var day = "0" + date.getDate();
-      var hour = "0" + date.getHours();
-      var minute = "0" + date.getMinutes();
+      let date = new Date(t*1000);
+      let year = date.getFullYear();
+      let month = "0" + (date.getMonth()+1);
+      let day = "0" + date.getDate();
+      let hour = "0" + date.getHours();
+      let minute = "0" + date.getMinutes();
       // var second = "0" + date.getSeconds();
       return year + "-" + month.substr(-2) + "-" + day.substr(-2) + " " + hour.substr(-2) + ":" + minute.substr(-2) + ":" 
       // + second.substr(-2);
     }
 
-    const Timeend = (t)=> {
-      return new Date(t.getTime() + (t.getTimezoneOffset() * 60000)); 
-    }
   
   
 
@@ -102,20 +93,30 @@ const ChatBotHome = ({navigation}:NavigationProp) => {
     //   e?.preventDefault(); // 버튼을 통한 제출이라면 새로고침 방지
     //   setText("");
     // };
+    function getClock(t){ // getClock 함수 생성
+      const hours = String(t.getHours()).padStart(2,"0"); // getHours() 시간을 받아옴 ,Number타입으로 받아온 것을 String으로 받아옴
+      const minutes = String(t.getMinutes()).padStart(2,"0"); //getMinutes() 분을 받아옴 ,Number타입으로 받아온 것을 String으로 받아옴
+      const seconds = String(t.getSeconds()).padStart(2,"0"); //getSeconds() 초를 받아옴 ,Number타입으로 받아온 것을 String으로 받아옴
+      return `${hours}:${minutes}:${seconds}`; //clock에 innerText로 넣어줬음;
+  }
 
 
 
     // const pressHandler = async () => {
+    //     setLoading(true);
+    //     setTextInput('');
     //     const formsubmit = {
-    //       'text':text,
+    //       'text':textInput,
     //     };
+    //     setLoading(false);
     //     const res = await getFormSubmit(formsubmit);
-    //     console.log("formsubmit?",formsubmit);
-    //     console.log("text?",text);
-    //     console.log("res.data 결과값 도출?",res.data);
+    //     // console.log("formsubmit?",formsubmit);
+    //     // console.log("res?",res);
+    //     // console.log("text?",textInput);
+    //     // console.log("res.data 결과값 도출?",res.data);
+    //     // console.log("res.headers.date?",res.headers.date);
+    //     setData([...data,{type:'user','text':textInput,'time':new Date(res.headers.date)},{type:'bot','text':res.data.resMsg,'time':new Date(res.headers.date)}])
     // };
-    
-
 //     tI7iIhm0
 // 비밀 키( REST API 호출에 사용 )
 // sk_test_hJCzFYorik8EAeKPj0AW3LxSHEf5yS8Q
@@ -133,7 +134,6 @@ const ChatBotHome = ({navigation}:NavigationProp) => {
                 <GlobalText>ChatBot</GlobalText>
           </HeadArea>
           <ContentArea>
-            
             <FlatList
                 style={{borderWidth:1,borderColor:theme.COLORS.BG_GRAY3,borderStyle:'solid'}}
                 ref={flatListRef}
@@ -146,21 +146,21 @@ const ChatBotHome = ({navigation}:NavigationProp) => {
                 keyExtractor={(item,idx)=>idx.toString()}
                 renderItem={({item})=>(
                     <View style={{padding:10}}>
-                    
-                      <View style={{display:'flex',flexDirection:'row' ,justifyContent:'space-between'}}>
-                        <Text style={{marginLeft:0,fontWeight:'bold',color:item.type === 'user' ? theme.COLORS.BLUE1 : theme.COLORS.RED}}>
-                          {item.type === 'user' ? `나:${submitDate}` : `ChatGPT${createDate}`}
-                        </Text>
-                        <Text style={{color:item.type === 'user' ? theme.COLORS.BLUE1 : theme.COLORS.RED}}>
-                          {/* {submitDate} */}
-                        </Text>
-                      </View>
+                        {item.type === 'user' ?
+                          <View style={{flex:1,flexDirection:'row',width:'100%',justifyContent:'space-between'}}>
+                            <Text style={{fontWeight:'bold',color:theme.COLORS.BLUE1}} >나:</Text>
+                            <Text style={{fontWeight:'bold',color:theme.COLORS.BLUE1,fontSize:12}} >{item.time}</Text>
+                          </View> :
+                            <View style={{flex:1,flexDirection:'row',width:'100%',justifyContent:'space-between'}}>
+                              <Text style={{fontWeight:'bold',color:theme.COLORS.RED}}>ChatGPT:</Text>
+                              <Text style={{fontWeight:'bold',color:theme.COLORS.RED,fontSize:12}}>{item.time}</Text>
+                          </View>
+                          }
+                        <Text style={{color:item.type === 'user' ? theme.COLORS.BLUE1 : theme.COLORS.RED}}></Text>
                       <Text style={{marginLeft:10,padding:10}}>{item.text}</Text>
                     </View>
                 )}
-                  >
-                    
-
+                >
             </FlatList>
               <View style={{paddingTop:30,paddingBottom:30}}>
                   {loading && <ActivityIndicator color={theme.COLORS.BLUE1}/>}
@@ -176,7 +176,6 @@ const ChatBotHome = ({navigation}:NavigationProp) => {
                   multiline={true}
                   numberOfLines={1}
                 />
-               
                 <TouchButton onPress={()=>handleSend()}>
                     <GlobalImage source={SubmitIco}/  >
                 </TouchButton>
